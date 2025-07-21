4d5f228659565d5dd31f6dead2c34eceee567423
Mohit <mohitsankhyan253@gmail.com>
2025-07-21 12:47:03 +0530

    practice complete

---
+++ par1/src/comn1/Footer.jsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <footer>
+      <h1>Jai Mahakal</h1>
+    </footer>
+  )
+}
+
+export default Footer;
---
+++ par1/src/comn1/Main.jsx
@@ -0,0 +1,10 @@
+const Main = () => {
+  return (
+    <main>
+      <h2>Main content</h2>
+      <p>This is main content</p>
+    </main>
+  )
+}
+
+export default Main;
