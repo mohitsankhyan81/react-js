63f18b119d0540b87e241e3284dcf1894cb25468
Mohit <mohitsankhyan253@gmail.com>
2025-07-22 09:43:28 +0530

    jsx rules

---
+++ practice1/src/App.jsx
@@ -0,0 +1,9 @@
+import JsxRule from './comn1/JsxRule'
+
+const App = () => {
+  return (
+    <JsxRule/>
+  )
+}
+
+export default App
---
+++ practice1/src/comn1/JsxRule.jsx
@@ -0,0 +1,9 @@
+const JsxRule = () => {
+  return (
+    <div>
+      <h1>hello world my name is mohit sankhyan</h1>
+    </div>
+  )
+}
+
+export default JsxRule;
