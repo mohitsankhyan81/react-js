c27c5e1021ccd64cb877cffcbfe47c66eb8e3652
Mohit <mohitsankhyan253@gmail.com>
2025-07-22 10:00:30 +0530

    completed

--- practice2/src/Prac1/Jsx.jsx
+++ practice2/src/Prac1/Jsx.jsx
@@ -1,11 +1,16 @@
 const Jsx = () => {
   const name="mohit Sankhyan";
+  const multiply=(a,b)=> a*b
+  const spectialclass='myname';
   return (
     <div>
       <p>2+2 = {2+2}</p>
       <h1>My name is {name}</h1>
+      <p>My frind list {[' alex ','john ','mintu ']}</p>
+      <p>a * b = {multiply(2,3)}</p>
+      <p className={spectialclass}>this is the special class</p>
     </div>
   )
 }
 
-export default Jsx
+export default Jsx;
