927fe4183b6bbe58b6b89b5a43280808d6f95ec1
Mohit <mohitsankhyan253@gmail.com>
2025-07-23 20:02:49 +0530

    done

---
+++ props1/src/App.jsx
@@ -0,0 +1,14 @@
+import Cart from "./comp/Cart.jsx";
+
+const App = () => {
+  return (
+    <div>
+      <Cart>
+        <h1>My Cart</h1>
+        <p>Here is some content for cart 1</p>
+      </Cart>
+    </div>
+  );
+};
+
+export default App;
---
+++ props1/src/comp/Cart.jsx
@@ -0,0 +1,9 @@
+const Cart = (props) => {
+  return (
+    <div>
+      {props.children}
+    </div>
+  )
+}
+
+export default Cart
