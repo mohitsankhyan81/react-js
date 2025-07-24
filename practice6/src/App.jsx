f749703de7003c91f72326d52b5eb9ae8b8d8990
Mohit <mohitsankhyan253@gmail.com>
2025-07-24 12:17:49 +0530

    commit

--- practice6/src/App.jsx
+++ practice6/src/App.jsx
@@ -1,9 +1,13 @@
+import Greet from "./comp/Greet"
+import Userstatus from "./comp/Userstatus"
 import Weather from "./comp/Weather"
 
 const App = () => {
   return (
     <div>
       <Weather/>
+      <Userstatus loggin={true} isadmin={true}/>
+      <Greet timeofday="morning"/>
     </div>
   )
 }
--- practice6/src/comp/Userstatus.jsx
+++ practice6/src/comp/Userstatus.jsx
@@ -1,7 +1,12 @@
-const Userstatus = () => {
-  return (
-    <div>Userstatus</div>
-  )
+const Userstatus = (props) => {
+
+  if(props.loggin && props.isadmin){
+    return <h1>Welcome Addmin</h1>
+  }
+  else{
+    return <h1>welcome user</h1>
+  }
+
 }
 
 export default Userstatus
