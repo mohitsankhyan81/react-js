221d53944ac50e0577b073dd6043369a22a61e7a
Mohit <mohitsankhyan253@gmail.com>
2025-07-25 20:27:19 +0530

    commit

--- style/style3/src/App.jsx
+++ style/style3/src/App.jsx
@@ -1,6 +1,12 @@
+import './index.css'
+import { FaCartArrowDown } from "react-icons/fa";
+
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+      <h1>Seprate file for styleing</h1>
+      <FaCartArrowDown/>
+    </div>
   )
 }
 
