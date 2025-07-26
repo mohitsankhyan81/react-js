3d7573fe6f4b4f6d93f282fba17696c49ddbef9a
Mohit <mohitsankhyan253@gmail.com>
2025-07-27 05:01:26 +0530

    commit

--- technical/tech2/src/App.jsx
+++ technical/tech2/src/App.jsx
@@ -1,7 +1,27 @@
-const App = () => {
-  return (
-    <div>App</div>
-  )
-}
+import { useState } from "react";
 
-export default App
+const App = () => {
+  const [friends, setFriends] = useState(["Alex", "John"]);
+
+  const addOneFriend = () => setFriends([...friends, "Mohit Sankhyan","krish"]);
+  const removeFriend = () => setFriends(friends.filter(f => f !== "John"));
+  const updateonefriend=()=>{
+    setFriends(friends.map(f=>f === "Alex"?"Alex Jhon":f))
+  }
+
+  return (
+    <div>
+      <ul>
+        {friends.map(friend => (
+          <li key={friend}>{friend}</li>
+        ))}
+      </ul>
+
+      <button onClick={addOneFriend}>Add New Friend</button>
+      <button onClick={removeFriend}>Remove Friend</button>
+      <button onClick={updateonefriend}>Update one friend</button>
+    </div>
+  );
+};
+
+export default App;
