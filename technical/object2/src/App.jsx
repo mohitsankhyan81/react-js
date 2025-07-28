a14c3b967a580feb25e9fb15d2aa5b5298fcc447
mohitsankhyan81 <mohitsankhyan253@gmail.com>
2025-07-28 12:42:52 +0530

    commit

--- technical/object2/src/App.jsx
+++ technical/object2/src/App.jsx
@@ -1,6 +1,13 @@
+import { useState } from "react"
+import CompOne from "./comp/CompOne";
+import CompTwo from "./comp/CompTwo";
 const App = () => {
+  const [count,serCount]=useState(0);
   return (
-    <div>App</div>
+    <div>
+      <CompOne count={count} onClickHandler={()=>serCount(count+1)}/>
+      <CompTwo count={count} onClickHandler={()=>serCount(count+1)}/>
+    </div>
   )
 }
 
