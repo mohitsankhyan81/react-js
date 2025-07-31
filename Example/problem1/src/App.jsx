74afee87c7a133a77ba4ef35992533c094e9448a
mohitsankhyan81 <mohitsankhyan253@gmail.com>
2025-07-31 11:14:52 +0530

    commit

--- Example/problem1/src/App.jsx
+++ Example/problem1/src/App.jsx
@@ -1,10 +1,12 @@
 import Count from "./comp/Count"
+import Profile from "./comp/Profile"
 import Todolist from "./comp/Todolist"
 const App = () => {
   return (
     <div>
       <Count/>
       <Todolist/>
+      <Profile/>
     </div>
   )
 }
