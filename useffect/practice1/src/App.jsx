34ab2253254d94d6dcc901c2d4530e3f90953583
mohitsankhyan81 <mohitsankhyan253@gmail.com>
2025-08-01 20:46:28 +0530

    commit

--- useffect/practice1/src/App.jsx
+++ useffect/practice1/src/App.jsx
@@ -1,11 +1,13 @@
 import BasicEffect from "./comp/BasicEffect"
 import CounterEffect from "./comp/CounterEffect"
+import Fetchdata from "./comp/Fetchdata"
 
 const App = () => {
   return (
     <div>
       <BasicEffect/>
       <CounterEffect/>
+      <Fetchdata/>
     </div>
   )
 }
