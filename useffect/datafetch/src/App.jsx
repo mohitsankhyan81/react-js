72a24b7fdf2f3b203c128e5bbe442309bb20e79b
mohitsankhyan81 <mohitsankhyan253@gmail.com>
2025-08-01 20:07:50 +0530

    commit

--- useffect/datafetch/src/App.jsx
+++ useffect/datafetch/src/App.jsx
@@ -1,8 +1,25 @@
-import React from 'react'
+import { useState , useEffect} from "react"
 
 const App = () => {
+
+  const [data,setdata]=useState([]);
+  useEffect(()=>{
+    async function getData(){
+      const resourse=await fetch("https://jsonplaceholder.typicode.com/todos");
+      const data =await resourse.json();
+     
+      if(data && data.length)setdata(data);
+
+    } 
+    getData();
+  },[]);
+
   return (
-    <div>App</div>
+    <div>
+      <ul>{data.map(todo=>{
+       return  <li key={todo.id}>{todo.title}</li>
+      })}</ul>
+    </div>
   )
 }
 
