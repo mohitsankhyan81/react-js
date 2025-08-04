57ce8401f11c291a5bc6d232bd7e5653d2df3584
mohitsankhyan81 <mohitsankhyan253@gmail.com>
2025-08-04 12:37:35 +0530

    commit

--- portal/props1/src/const/Popupcontent.jsx
+++
@@ -1,9 +0,0 @@
-const Popupcontent = ({copy}) => {
-  return (
-    <section>
-      {copy && <div style={{position:"absolute" ,bottom:"3rem"}}>Copy to ClipBoard</div>}
-    </section>
-  )
-}
-
-export default Popupcontent
