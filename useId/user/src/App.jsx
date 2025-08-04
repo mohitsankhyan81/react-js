49400460c7502b8c1df5a312f56572ea7e9dc9fd
mohitsankhyan81 <mohitsankhyan253@gmail.com>
2025-08-04 19:21:42 +0530

    commit

---
+++ useId/user/src/App.jsx
@@ -0,0 +1,15 @@
+import UseId from "./comp/UseId"
+
+const App = () => {
+  return (
+    <div>
+      <UseId/>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Officiis animi vero ipsam architecto, beatae error aperiam voluptatibus sit, et laborum consequuntur similique nisi? Iure ea sunt non, explicabo ipsum quas.
+      </p>
+      <UseId/>
+    </div>
+  )
+}
+
+export default App
