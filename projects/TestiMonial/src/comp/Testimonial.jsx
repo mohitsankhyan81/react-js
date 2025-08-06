b9de1e03a9819f937829c33bae5755fd0a03fd8e
mohitsankhyan81 <mohitsankhyan253@gmail.com>
2025-08-06 20:29:14 +0530

    commit

--- projects/TestiMonial/src/comp/Testimonial.jsx
+++ projects/TestiMonial/src/comp/Testimonial.jsx
@@ -1,9 +1,6 @@
+import {useState} from "react"
+import "../style.css"
 
-
-const Testimonial = () => {
-  return (
-    <div>Testimonial</div>
-  )
-}
+const Testimonial=()=>{const[currentIndex,setCurrentIndex]=useState(0);const testimonial=[{quote:"this is the best project I've even used",author:"Jane Doe"},{quote:"this is my high recommended project and I am working on it",author:"Jane Doe"},{quote:"we are playing free fire daily and enjoy my life",author:"Jane Doe"},{quote:"we are learning useState from many days and I love to code in JSX",author:"Jane Doe"}];const handlePrevClick=()=>{setCurrentIndex((currentIndex+testimonial.length-1)%testimonial.length)};const handleNextClick=()=>{setCurrentIndex((currentIndex+1)%testimonial.length)};return(<div className="testimonial"><div className="testimonial-quote">{testimonial[currentIndex].quote}</div><div className="testimonial-author">-{testimonial[currentIndex].author}</div><div className="testimonial-nav"><button onClick={handlePrevClick}>Prev</button><button onClick={handleNextClick}>Next</button></div></div>)}
 
 export default Testimonial
