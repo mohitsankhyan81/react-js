d4fb8232f11398f07de25c88c5a1f2c67d640706
mohitsankhyan81 <mohitsankhyan253@gmail.com>
2025-08-15 22:44:41 +0530

    commit

--- projects/web1/src/SideBar/Category/Category.js
+++ projects/web1/src/SideBar/Category/Category.js
@@ -3,7 +3,13 @@ import './Category.css'
 function Category() {
   return (
     <div>
-      Category
+      <h2 className='sidebar-title'>Category</h2>
+      <div>
+        <label className='sidebar-label-container'>
+          <input type='radio' name='test'/>
+          <span className='checkmark'></span>All
+        </label>
+      </div>
     </div>
   )
 }
