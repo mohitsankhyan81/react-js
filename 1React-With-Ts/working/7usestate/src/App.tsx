3ff0a957f2a801abe0d5593be6ea5334235187ef
mohitsankhyan81 <mohitsankhyan253@gmail.com>
2025-08-28 19:31:14 +0530

    commit

---
+++ 1React-With-Ts/working/7usestate/src/App.tsx
@@ -0,0 +1,10 @@
+import Button from './comp/Button';
+const App = () => {
+  return (
+    <div>
+      <Button/>
+    </div>
+  )
+}
+
+export default App
---
+++ 1React-With-Ts/working/7usestate/src/comp/Button.tsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+
+const Button = () => {
+  const [count,setcount]=useState(0);
+  return (
+    <div>
+      <h1>Item Count -</h1>
+      <p>Count: {count}</p>
+      <button onClick={()=>setcount(count+1)}>Increment</button>
+      <button onClick={()=>setcount(count-1)}>Dicrement</button>
+    </div>
+  )
+}
+
+export default Button
