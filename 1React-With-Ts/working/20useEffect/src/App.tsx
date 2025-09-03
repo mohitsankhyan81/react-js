0c65d2084d30b4b128eca19628f5af28ad850e2d
mohitsankhyan81 <mohitsankhyan253@gmail.com>
2025-09-03 16:59:55 +0530

    commit

---
+++ 1React-With-Ts/working/20useEffect/src/App.tsx
@@ -0,0 +1,11 @@
+import Userlist from "./comp/Userlist"
+
+const App = () => {
+  return (
+    <div>
+      <Userlist/>
+    </div>
+  )
+}
+
+export default App
---
+++ 1React-With-Ts/working/20useEffect/src/comp/Userlist.tsx
@@ -0,0 +1,64 @@
+import { useEffect, useState } from "react"
+
+
+interface User{
+  id:number;
+  name:string;
+  username:string;
+  email:string;
+  phone:number;
+}
+const Userlist = () => {
+  const [users,setusers]=useState<User[]>([]);
+  const [loading,setloading]=useState<boolean>(true);
+  const [error,seterror]=useState<string|null|number>(null);
+
+  useEffect(()=>{
+    const fetchUsers=async()=>{
+      try{
+        const response= await fetch("https://jsonplaceholder.typicode.com/users")
+        if(!response.ok){
+          throw new Error('Network response is not ok')
+        }
+       const data:User[]=await response.json()
+       setusers(data);
+      }
+      catch(error){
+        seterror(error instanceof Error? error.message: "An error accor")
+      }
+      finally{
+        setloading(false);
+      }
+    }
+    fetchUsers()
+  },[])
+
+  if(loading)return<>loding...</>
+  if(error) return<>Error: {error}</>
+
+  return (
+    <div>
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Username</th>
+          <th>Email</th>
+          <th>Phone</th>
+        </tr>
+      </thead>
+      <tbody>
+        {users.map(user=>(
+          <tr key={user.id}>
+            <td>{user.id}</td>
+            <td>{user.name}</td>
+            <td>{user.username}</td>
+            <td>{user.email}</td>
+            <td>{user.phone}</td>
+          </tr>
+        ))}
+      </tbody>
+    </div>
+  )
+}
+
+export default Userlist
