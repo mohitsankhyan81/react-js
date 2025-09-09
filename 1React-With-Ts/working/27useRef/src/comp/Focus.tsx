676dfdcf4354979c1f2427840158d4f7cdc448d0
mohitsankhyan81 <mohitsankhyan253@gmail.com>
2025-09-09 23:45:09 +0530

    commit

---
+++ 1React-With-Ts/working/27useRef/src/comp/Focus.tsx
@@ -0,0 +1,17 @@
+import { useRef } from "react"
+
+const Focus = () => {
+  const inputref=useRef<HTMLInputElement>(null);
+
+  const handleclick=()=>{
+    inputref.current!.focus();
+  }
+  return (
+    <div>
+      <input type="text" placeholder="Click on focus" ref={inputref}/>
+      <input type="submit" onClick={handleclick} />
+    </div>
+  )
+}
+
+export default Focus
