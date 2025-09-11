dfd6c146bb76ca5596ca5b3fa7648584b27e795b
mohitsankhyan81 <mohitsankhyan253@gmail.com>
2025-09-11 13:31:59 +0530

    commit

---
+++ 1React-With-Ts/working/31useState/src/App.tsx
@@ -0,0 +1,34 @@
+import { useState } from "react"
+
+const App = () => {
+
+  const [input,setinput] = useState("");
+  const [todo,settodo] =useState([]);
+
+  const addtodolist=()=>{
+    const item={
+      id:todo.length+1,
+      text:input,
+      completed:false
+    }
+
+    settodo(prev=> [...prev,item]);
+    setinput("");
+  }
+  return (
+    <div>
+      <input type="text" placeholder="add list" value={input} onChange={(e)=>setinput(e.target.value)}/>
+      <button onClick={()=>addtodolist()}>Add</button>
+
+      <ul>
+        {todo.map(t=>(
+          <li key={t.id}>
+            <span>{t.text}</span>
+          </li>
+        ))}
+      </ul>
+    </div>
+  )
+}
+
+export default App
